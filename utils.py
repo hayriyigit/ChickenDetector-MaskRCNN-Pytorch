45d7e68d0a1d3cae879ed7901544ab633266030b
hayriyigit <hayriyigit@protonmail.com>
2022-11-04 12:12:57 +0300

    add utils

--- utils.py
+++ utils.py
@@ -1,4 +1,19 @@
-# load checkpoint
-# save checkpoint
+import torch
+
+def load_checkpoint(checkpoint, model, optimizer, lr):
+    print("=> Loading checkpoint")
+    model.load_state_dict(checkpoint["state_dict"])
+    #optimizer.load_state_dict(checkpoint["optimizer"])
+
+    # If we don't do this then it will just have learning rate of old checkpoint
+    # and it will lead to many hours of debugging \:
+    for param_group in optimizer.param_groups:
+        param_group["lr"] = lr
+
+def save_checkpoint(state, filename="mask_rcnn.pth.tar"):
+    print("=> Saving checkpoint")
+    torch.save(state, filename)
+
+
 # predict single frame
 # predict on video
